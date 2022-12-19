1c4a10948d22d89244c8e6b0b6283ed59df0f727
natesway <64743334+natesway@users.noreply.github.com>
2022-12-19 16:17:21 -0500

    Update build.py

--- build.py
+++ build.py
@@ -97,7 +97,7 @@ def bootstrap_rootfs() -> None:
     chroot("dnf group install -y 'Common NetworkManager Submodules'")
     chroot("dnf install -y linux-firmware")
     chroot("dnf install -y git vboot-utils rsync cloud-utils parted grub2-efi-x64 efibootmgr")  # postinstall dependencies
-    chroot("dnf install -y grub2-efi-x64-modules")
+    chroot("dnf install -y grub2-efi-x64-modules grub2-efi grub2-efi-modules shim")
 
     # Add RPMFusion repos
     chroot(f"dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-37.noarch.rpm")
@@ -205,6 +205,8 @@ def configure_rootfs(uuids) -> None:
         fstab = f"\nUUID={uuids[0]} /boot vfat rw,relatime,fmask=0022,dmask=0022,codepage=437 0 2\n{uuids[1]} / ext4 rw,relatime 0 1"
 
     # Install grub
+    chroot("grub2-mkconfig -o /boot/grub/grub.cfg")
+    chroot("grub2-mkconfig -o /boot/grub2/grub.cfg")
     chroot("grub2-install --target=x86_64-efi --efi-directory=/boot --removable")
     chroot("grub2-mkconfig -o /boot/grub/grub.cfg")
 
